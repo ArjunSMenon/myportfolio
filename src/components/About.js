99ef70c58a2ef44b8b24840f56392817a22d82b1
ArjunSMenon <arjun.smenon.95@gmail.com>
2024-12-06 12:54:12 +0530

    created components folder for different pages

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const About = () => {
+
+  return (
+    <div className="About">
+      <h1>About Me</h1>
+      <p>I am a developer </p>
+    </div>
+  )
+}
+
+export default About
