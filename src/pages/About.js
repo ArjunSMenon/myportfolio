8c1ed3fbf9beccd8727c81f32071bfcfd3038b4b
ArjunSMenon <arjun.smenon.95@gmail.com>
2024-12-09 19:01:43 +0530

    added header about contact and home pages

---
+++ src/pages/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./About.css";
+
+const About = () => {
+  return (
+    <section id="about" className="about">
+      <h2>About Me</h2>
+      <p>I am a backend developer with expertise in Node.js, Express.js and building RESTful APIs. I focus on creating efficient and scsalable applications</p>
+    </section>
+  );
+};
+
+export default About;
