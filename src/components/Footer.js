5e18816a4ff6cac619c6ae05668516af11cb7b30
ArjunSMenon <arjun.smenon.95@gmail.com>
2024-12-10 12:54:54 +0530

    removed background image and added a footer with copyright and email link

---
+++ src/components/Footer.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <nav className="horizontal-list">
+        <div className="copyright">
+          © 2024 Arjun S Menon
+        </div>
+        <a href="mailto:arjun.smenon.95@gmail.com" className="contact-link">
+          arjun.smenon.95@gmail.com
+        </a>
+      </nav>
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="#projects">Projects</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+    </footer>
+  )
+}
+
+export default Footer;
--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,6 @@ const Header = () => {
       <nav className="horizontal-list">
         <ul>
           <li><a href="/">Home</a></li>
-          <li><a href="#about">About</a></li>
           <li><a href="#projects">Projects</a></li>
           <li><a href="#contact">Contact</a></li>
         </ul>
